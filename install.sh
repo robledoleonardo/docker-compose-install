56c9af6e1384e293e4c8a40232ba2e160226ff93
Robledo Leonardo <robledo.leonardo@gmail.com>
2019-07-10 17:07:23 -0300

    permission user docker

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ apt update
 apt-cache policy docker-ce
 apt install docker-ce
 usermod -aG docker ${USER}
+su - ${USER}
 echo "docker-compose install"
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
